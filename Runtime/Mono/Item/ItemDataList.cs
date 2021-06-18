e8c328ae348460c05d3c7505dbb7e8f85ad8b492
Syadeu <syadeu@gmail.com>
2021-06-18 19:08:52 +0900

    added moonsharp for lua implement https://www.moonsharp.org/unitytips.html

--- Runtime/Mono/Item/ItemDataList.cs
+++ Runtime/Mono/Item/ItemDataList.cs
@@ -5,6 +5,8 @@ namespace Syadeu.Database
     [PreferBinarySerialization][CustomStaticSetting("Syadeu/Item")]
     public sealed class ItemDataList : StaticSettingEntity<ItemDataList>
     {
+        public override bool RuntimeModifiable => base.RuntimeModifiable;
+
         public Item[] m_Items;
         public ItemType[] m_ItemTypes;
         public ItemEffectType[] m_ItemEffectTypes;
