125610b7aa72007f8d5b364c7ff7510039ee156f
Syadeu <syadeu@g1playground.com>
2021-06-28 01:25:44 +0900

    스위치 버그 수정

--- Runtime/Mono/Simple/SimpleSwitch.cs
+++ Runtime/Mono/Simple/SimpleSwitch.cs
@@ -26,34 +26,40 @@ namespace Syadeu.Mono
         {
             for (int i = 0; i < m_Contents.Length; i++)
             {
-                if (m_Contents[i].m_Default)
+                if (m_Contents[i].m_Object == null) continue;
+
+                if (m_Contents[i].m_Object is GameObject gameObj)
                 {
-                    On(i);
+                    gameObj.SetActive(m_Contents[i].m_Default);
                 }
-                else
+                else if (m_Contents[i].m_Object is Behaviour behaviour)
                 {
-                    Off(i);
+                    behaviour.enabled = m_Contents[i].m_Default;
                 }
+                else throw new CoreSystemException(CoreSystemExceptionFlag.Mono,
+                    $"지원하지 않는 오브젝트 타입 ({m_Contents[i].m_Object.GetType().Name})");
+
+                m_Contents[i].m_Enabled = m_Contents[i].m_Default;
             }
         }
 
-        public void Toogle(int idx)
-        {
-            if (m_Contents[idx].m_Object is GameObject gameObj)
-            {
-                gameObj.SetActive(!gameObj.activeInHierarchy);
-            }
-            else if (m_Contents[idx].m_Object is Behaviour behaviour)
-            {
-                behaviour.enabled = !behaviour.enabled;
-            }
-            else throw new CoreSystemException(CoreSystemExceptionFlag.Mono,
-                $"지원하지 않는 오브젝트 타입 ({m_Contents[idx].m_Object.GetType().Name})");
-        }
+        //public void Toogle(int idx)
+        //{
+        //    if (m_Contents[idx].m_Object is GameObject gameObj)
+        //    {
+        //        gameObj.SetActive(!gameObj.activeInHierarchy);
+        //    }
+        //    else if (m_Contents[idx].m_Object is Behaviour behaviour)
+        //    {
+        //        behaviour.enabled = !behaviour.enabled;
+        //    }
+        //    else throw new CoreSystemException(CoreSystemExceptionFlag.Mono,
+        //        $"지원하지 않는 오브젝트 타입 ({m_Contents[idx].m_Object.GetType().Name})");
+        //}
 
         public void On(int idx)
         {
-            if (!m_Contents[idx].m_Enabled)
+            if (!m_Contents[idx].m_Enabled && m_Contents[idx].m_Object != null)
             {
                 if (m_Contents[idx].m_Object is GameObject gameObj)
                 {
@@ -78,7 +84,7 @@ namespace Syadeu.Mono
         }
         public void Off(int idx)
         {
-            if (m_Contents[idx].m_Enabled)
+            if (m_Contents[idx].m_Enabled && m_Contents[idx].m_Object != null)
             {
                 if (m_Contents[idx].m_Object is GameObject gameObj)
                 {
