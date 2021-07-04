5863840965f99b85d480cd439c24bd96deb4e3a4
Syadeu <syadeu@g1playground.com>
2021-07-05 00:16:05 +0900

    주석 추가

--- Runtime/Presentation/PresentationRegisterEntity.cs
+++ Runtime/Presentation/PresentationRegisterEntity.cs
@@ -1,8 +1,15 @@
 ﻿namespace Syadeu.Presentation
 {
     /// <summary>
-    /// <see cref="PresentationManager"/>에게 시스템을 등록하기 위한 레지스터 클래스입니다.
+    /// <see cref="PresentationManager"/>에게 시스템을 등록하기 위한 레지스터 클래스입니다. 참조한 클래스는 그룹이 됩니다.
     /// </summary>
+    /// <remarks>
+    /// 시스템 그룹으로 묶을 클래스에 이 <see langword="abstract"/>를 참조하고, 
+    /// <see cref="Register"/> 메소드에서 <see cref="RegisterSystem(System.Type[])"/>을 호출하여 시스템 그룹으로 묶을 수 있습니다.<br/>
+    /// 등록할 시스템 객체는 <see cref="PresentationSystemEntity{T}"/>를 참조하여야 합니다.<br/>
+    /// <br/>
+    /// 묶은 시스템은 이후 <see cref="PresentationSystemGroup{T}"/>로 그룹을 호출할 수 있습니다.
+    /// </remarks>
     public abstract class PresentationRegisterEntity : IPresentationRegister
     {
         public virtual SceneReference DependenceScene => null;
