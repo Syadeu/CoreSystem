a33ee3bc306ea9246d8e1bc2a0cfdfcb8266da9d
Syadeu <syadeu@gmail.com>
2021-07-21 14:20:43 +0900

    u

--- Runtime/Database/BinaryGrid.cs
+++ Runtime/Database/BinaryGrid.cs
@@ -136,12 +136,13 @@ namespace Syadeu.Database
             m_Extents = new float3(size) * .5f;
         }
 
+#pragma warning disable IDE1006 // Naming Styles
         public Vector3 center { get { return m_Center; } set { m_Center = value; } }
         public Vector3 size { get { return m_Extents * 2.0F; } set { m_Extents = value * 0.5F; } }
         public Vector3 extents { get { return m_Extents; } set { m_Extents = value; } }
         public Vector3 min { get { return center - extents; } set { SetMinMax(value, max); } }
         public Vector3 max { get { return center + extents; } set { SetMinMax(min, value); } }
-
+#pragma warning restore IDE1006 // Naming Styles
         public void SetMinMax(Vector3 min, Vector3 max)
         {
             extents = (max - min) * 0.5F;
