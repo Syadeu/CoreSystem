e005f6894d245075d6f1dc3bd346dcb4269ea17c
Syadeu <syadeu@gmail.com>
2021-08-10 18:09:19 +0900

    시스템 정리 최적화

---
+++ Runtime/Presentation/RenderSystem/CameraData.cs
@@ -0,0 +1,36 @@
+﻿#undef UNITY_ADDRESSABLES
+
+#if UNITY_ADDRESSABLES
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.ResourceManagement.AsyncOperations;
+#endif
+
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Syadeu.Presentation.Render
+{
+    public struct CameraData
+    {
+		public float3 position;
+		public quaternion orientation;
+		public float fov;
+		public float nearClipPlane;
+		public float farClipPlane; 
+		public float aspect;
+
+		public CameraData(Camera cam)
+        {
+			CoreSystem.Logger.ThreadBlock(nameof(CameraData), Syadeu.Internal.ThreadInfo.Unity);
+			Transform tr = cam.transform;
+
+			position = tr.position;
+			orientation = tr.rotation;
+			fov = cam.fieldOfView;
+			nearClipPlane = cam.nearClipPlane;
+			farClipPlane = cam.farClipPlane;
+			aspect = cam.aspect;
+        }
+	}
+}
