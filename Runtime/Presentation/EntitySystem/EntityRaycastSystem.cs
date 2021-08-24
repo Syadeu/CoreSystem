f21aef8b995785341ac4ebe874c60a6817a17140
Syadeu <syadeu@gmail.com>
2021-08-24 19:22:24 +0900

    u

--- Runtime/Presentation/EntitySystem/EntityRaycastSystem.cs
+++ Runtime/Presentation/EntitySystem/EntityRaycastSystem.cs
@@ -5,5 +5,27 @@
         public override bool EnableBeforePresentation => false;
         public override bool EnableOnPresentation => false;
         public override bool EnableAfterPresentation => false;
+
+        private GameObjectProxySystem m_ProxySystem;
+
+        protected override PresentationResult OnInitialize()
+        {
+            RequestSystem<GameObjectProxySystem>(Bind);
+
+            return base.OnInitialize();
+        }
+        private void Bind(GameObjectProxySystem other)
+        {
+            m_ProxySystem = other;
+
+            m_ProxySystem.OnDataObjectVisible += M_ProxySystem_OnDataObjectVisible;
+            m_ProxySystem.OnDataObjectInvisible += M_ProxySystem_OnDataObjectInvisible;
+        }
+        private void M_ProxySystem_OnDataObjectVisible(ProxyTransform obj)
+        {
+        }
+        private void M_ProxySystem_OnDataObjectInvisible(ProxyTransform obj)
+        {
+        }
     }
 }
