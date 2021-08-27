c38e2dda0e2857d1c18fb9c970da4e7c82d9a3b5
Syadeu <syadeu@gmail.com>
2021-08-27 15:29:29 +0900

    인풋 시스템 작성중

--- Runtime/Presentation/InputSystem/InputSystem.cs
+++ Runtime/Presentation/InputSystem/InputSystem.cs
@@ -1,85 +1,96 @@
-﻿//using Syadeu.Internal;
-//using System;
-//using UnityEngine;
-//using UnityEngine.InputSystem;
-//using UnityEngine.InputSystem.Controls;
-//using UnityEngine.InputSystem.LowLevel;
+﻿using Syadeu.Internal;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.LowLevel;
 
-//namespace Syadeu.Presentation.Input
-//{
-//    public sealed class InputSystem : PresentationSystemEntity<InputSystem>
-//    {
-//        public override bool EnableBeforePresentation => false;
-//        public override bool EnableOnPresentation => false;
-//        public override bool EnableAfterPresentation => false;
+namespace Syadeu.Presentation.Input
+{
+    public sealed class InputSystem : PresentationSystemEntity<InputSystem>
+    {
+        public override bool EnableBeforePresentation => false;
+        public override bool EnableOnPresentation => false;
+        public override bool EnableAfterPresentation => false;
 
-//        protected override PresentationResult OnInitialize()
-//        {
-//            //global::UnityEngine.InputSystem.InputSystem.RegisterInteraction()
+        private bool m_Enable = true;
+        private InputActionAsset m_InputActions;
 
-//            return base.OnInitialize();
-//        }
-//        private void asdasd()
-//        {
-//            InputAction inputAction = new InputAction();
-//            inputAction.bindingMask = new InputBinding();
+        protected override PresentationResult OnInitialize()
+        {
+            m_InputActions = InputSystemSettings.Instance.m_InputActions;
+            if (m_InputActions == null)
+            {
+                m_Enable = false;
+                return PresentationResult.Warning("InputActions is not set. InputSystem will be offline.");
+            }
 
-//            //InputControlPath.TryFindControl<>(Gamepad.current, "leftStick/x");
-//        }
+            m_InputActions.Enable();
+            //global::UnityEngine.InputSystem.InputSystem.RegisterInteraction()
 
-//        public static InputControl ToControlType(ControlType controlType)
-//        {
-//            if (controlType == ControlType.Keyboard) return Keyboard.current;
-//            else if (controlType == ControlType.Mouse) return Mouse.current;
-//            else if (controlType == ControlType.Gamepad) return Gamepad.current;
+            return base.OnInitialize();
+        }
+        private void asdasd()
+        {
+            InputAction inputAction = new InputAction();
+            inputAction.bindingMask = new InputBinding();
 
-//            return null;
-//        }
-//    }
-//    public struct KeyboardBinding : IEquatable<KeyboardBinding>
-//    {
-//        public ControlType ControlType => ControlType.Keyboard;
+            //InputControlPath.TryFindControl<>(Gamepad.current, "leftStick/x");
+        }
 
-//        public Key Key;
+        public static InputControl ToControlType(ControlType controlType)
+        {
+            if (controlType == ControlType.Keyboard) return Keyboard.current;
+            else if (controlType == ControlType.Mouse) return Mouse.current;
+            else if (controlType == ControlType.Gamepad) return Gamepad.current;
 
-//        public KeyControl ToControl() => Keyboard.current[Key];
-//        public bool Equals(KeyboardBinding other) => Key.Equals(other.Key);
-//        public override string ToString() => InputControlPath.Combine(InputSystem.ToControlType(ControlType), TypeHelper.Enum<Key>.ToString(Key));
-//    }
-//    public struct MouseBinding
-//    {
-//        public ControlType ControlType => ControlType.Mouse;
+            return null;
+        }
+    }
+    public struct KeyboardBinding : IEquatable<KeyboardBinding>
+    {
+        public ControlType ControlType => ControlType.Keyboard;
 
-//        public MouseButton MouseButton;
+        public Key Key;
 
-//        public InputControl ToControl() => InputControlPath.TryFindControl(Mouse.current, ToString());
-//        public override string ToString() => InputControlPath.Combine(InputSystem.ToControlType(ControlType), TypeHelper.Enum<MouseButton>.ToString(MouseButton));
-//    }
-//    public struct GamepadBinding
-//    {
-//        public ControlType ControlType => ControlType.Gamepad;
+        public KeyControl ToControl() => Keyboard.current[Key];
+        public bool Equals(KeyboardBinding other) => Key.Equals(other.Key);
+        public override string ToString() => InputControlPath.Combine(InputSystem.ToControlType(ControlType), TypeHelper.Enum<Key>.ToString(Key));
+    }
+    public struct MouseBinding
+    {
+        public ControlType ControlType => ControlType.Mouse;
 
-//        public GamepadButton GamepadButton;
+        public MouseButton MouseButton;
 
-//        public ButtonControl ToControl() => Gamepad.current[GamepadButton];
-//        public override string ToString() => InputControlPath.Combine(InputSystem.ToControlType(ControlType), TypeHelper.Enum<GamepadButton>.ToString(GamepadButton));
-//    }
+        public InputControl ToControl() => InputControlPath.TryFindControl(Mouse.current, ToString());
+        public override string ToString() => InputControlPath.Combine(InputSystem.ToControlType(ControlType), TypeHelper.Enum<MouseButton>.ToString(MouseButton));
+    }
+    public struct GamepadBinding
+    {
+        public ControlType ControlType => ControlType.Gamepad;
 
-//    public enum ControlType
-//    {
-//        Keyboard,
-//        Mouse,
-//        Gamepad,
-//    }
-//    public sealed class MyInteraction : IInputInteraction
-//    {
-//        public void Process(ref InputInteractionContext context)
-//        {
-            
-//        }
-//        public void Reset()
-//        {
-//            throw new System.NotImplementedException();
-//        }
-//    }
-//}
+        public GamepadButton GamepadButton;
+
+        public ButtonControl ToControl() => Gamepad.current[GamepadButton];
+        public override string ToString() => InputControlPath.Combine(InputSystem.ToControlType(ControlType), TypeHelper.Enum<GamepadButton>.ToString(GamepadButton));
+    }
+
+    public enum ControlType
+    {
+        Keyboard,
+        Mouse,
+        Gamepad,
+    }
+    public sealed class MyInteraction : IInputInteraction
+    {
+        public void Process(ref InputInteractionContext context)
+        {
+
+        }
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Runtime/Presentation/InputSystem/InputSystemSettings.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Syadeu.Database;
+using Syadeu.Presentation.Data;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Syadeu.Presentation.Input
+{
+    public sealed class InputSystemSettings : StaticSettingEntity<InputSystemSettings>
+    {
+        public InputActionAsset m_InputActions = null;
+    }
+}
