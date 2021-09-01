65aeb702b145bf6d15baee1b5d6b15f0b61e422f
Syadeu <syadeu@gmail.com>
2021-09-01 19:24:43 +0900

    스크립트 정리

--- Runtime/Presentation/EntitySystem/AttributeAcceptOnlyAttribute.cs
+++ Runtime/Presentation/EntitySystem/AttributeAcceptOnlyAttribute.cs
@@ -23,24 +23,4 @@ namespace Syadeu.Presentation
             Types = types;
         }
     }
-
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-    public sealed class EntityAcceptOnlyAttribute : Attribute
-    {
-        public Type[] AttributeTypes;
-
-        public EntityAcceptOnlyAttribute(params Type[] attributeTypes)
-        {
-            //for (int i = 0; i < attributeTypes.Length; i++)
-            //{
-            //    if (!TypeHelper.TypeOf<AttributeBase>.Type.IsAssignableFrom(attributeTypes[i]))
-            //    {
-            //        CoreSystem.Logger.LogError(Channel.Entity, $"Type({attributeTypes[i].Name}) is not a attribute type.");
-            //        throw new Exception();
-            //    }
-            //}
-            
-            AttributeTypes = attributeTypes;
-        }
-    }
 }
