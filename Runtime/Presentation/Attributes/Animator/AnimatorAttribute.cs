37ba1cbc5f6c225146dfc9586d575ecfe023b81a
Syadeu <syadeu@gmail.com>
2021-09-06 13:56:34 +0900

    애니메이터의 parameter 는 이제 프록시 패턴임

--- Runtime/Presentation/Attributes/Animator/AnimatorAttribute.cs
+++ Runtime/Presentation/Attributes/Animator/AnimatorAttribute.cs
@@ -20,25 +20,31 @@ namespace Syadeu.Presentation.Attributes
         [JsonProperty(Order = 0, PropertyName = "AnimationTrigger")]
         public Reference<AnimationTriggerAction>[] m_AnimationTriggers = Array.Empty<Reference<AnimationTriggerAction>>();
 
-        [JsonIgnore] internal AnimatorComponent Animator { get; set; }
+        [JsonIgnore] internal AnimatorComponent AnimatorComponent { get; set; }
         [JsonIgnore] internal Dictionary<int, object> Parameters { get; set; }
         [JsonIgnore] public Dictionary<Hash, List<Reference<AnimationTriggerAction>>> AnimationTriggers { get; internal set; }
 
+        public void SetInteger(int key, int value)
+        {
+            Parameters[key] = value;
+            if (AnimatorComponent != null) AnimatorComponent.m_Animator.SetInteger(key, value);
+        }
+        public int GetInteger(int key) => (int)Parameters[key];
+
         public void SetFloat(int key, float value)
         {
-            if (Animator == null) return;
-            Animator.m_Animator.SetFloat(key, value);
+            Parameters[key] = value;
+            if (AnimatorComponent != null) AnimatorComponent.m_Animator.SetFloat(key, value);
         }
-        public void SetFloat(string key, float value)
+        public void SetFloat(string key, float value) => SetFloat(Animator.StringToHash(key), value);
+        public float GetFloat(int key) => (float)Parameters[key];
+
+        public void SetBool(int key, bool value)
         {
-            if (Animator == null) return;
-            Animator.m_Animator.SetFloat(key, value);
+            Parameters[key] = value;
+            if (AnimatorComponent != null) AnimatorComponent.m_Animator.SetBool(key, value);
         }
-        public float GetFloat(int key)
-        {
-            if (Animator == null) return 0;
-            return Animator.m_Animator.GetFloat(key);
-        }
+        public bool GetBool(int key) => (bool)Parameters[key];
     }
     internal sealed class AnimatorProcessor : AttributeProcessor<AnimatorAttribute>,
         IAttributeOnProxy
@@ -46,6 +52,26 @@ namespace Syadeu.Presentation.Attributes
         protected override void OnCreated(AnimatorAttribute attribute, EntityData<IEntityData> entity)
         {
             attribute.Parameters = new Dictionary<int, object>();
+            Animator originAnimator = ((GameObject)((ProxyTransform)entity.As<IEntityData, IEntity>().transform).prefab.Asset).GetComponentInChildren<Animator>();
+            for (int i = 0; i < originAnimator.parameterCount; i++)
+            {
+                var param = originAnimator.GetParameter(i);
+                switch (param.type)
+                {
+                    case AnimatorControllerParameterType.Float:
+                        attribute.Parameters.Add(param.nameHash, originAnimator.GetFloat(param.nameHash));
+                        break;
+                    case AnimatorControllerParameterType.Int:
+                        attribute.Parameters.Add(param.nameHash, originAnimator.GetInteger(param.nameHash));
+                        break;
+                    case AnimatorControllerParameterType.Bool:
+                        attribute.Parameters.Add(param.nameHash, originAnimator.GetBool(param.nameHash));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
             attribute.AnimationTriggers = new Dictionary<Hash, List<Reference<AnimationTriggerAction>>>();
             for (int i = 0; i < attribute.m_AnimationTriggers.Length; i++)
             {
@@ -68,14 +94,21 @@ namespace Syadeu.Presentation.Attributes
 
         private static bool Setup(AnimatorAttribute att, Entity<IEntity> entity, RecycleableMonobehaviour monoObj)
         {
-            att.Animator = monoObj.GetComponent<AnimatorComponent>();
-            if (att.Animator == null)
+            att.AnimatorComponent = monoObj.GetComponent<AnimatorComponent>();
+            if (att.AnimatorComponent == null)
             {
                 CoreSystem.Logger.LogError(Channel.Entity,
                     $"This entity({entity.Name}) has {nameof(AnimatorAttribute)} but cannot found animator");
                 return false;
             }
-            att.Animator.m_AnimatorAttribute = att;
+            att.AnimatorComponent.m_AnimatorAttribute = att;
+
+            foreach (var item in att.Parameters)
+            {
+                if (item.Value is int integer) att.AnimatorComponent.m_Animator.SetInteger(item.Key, integer);
+                else if (item.Value is float single) att.AnimatorComponent.m_Animator.SetFloat(item.Key, single);
+                else if (item.Value is bool boolen) att.AnimatorComponent.m_Animator.SetBool(item.Key, boolen);
+            }
 
             return true;
         }
@@ -87,16 +120,16 @@ namespace Syadeu.Presentation.Attributes
                 return;
             }
 
-            att.Animator.SetActive(true);
+            att.AnimatorComponent.SetActive(true);
         }
 
         public void OnProxyRemoved(AttributeBase attribute, Entity<IEntity> entity, RecycleableMonobehaviour monoObj)
         {
             AnimatorAttribute att = (AnimatorAttribute)attribute;
 
-            att.Animator.SetActive(false);
-            att.Animator.m_AnimatorAttribute = null;
-            att.Animator = null;
+            att.AnimatorComponent.SetActive(false);
+            att.AnimatorComponent.m_AnimatorAttribute = null;
+            att.AnimatorComponent = null;
         }
     }
 }
