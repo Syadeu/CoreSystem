28694da59a43b515da7df4903e592d2fccd4a856
Syadeu <syadeu@g1playground.com>
2021-09-15 02:50:38 +0900

    ui orientation 추가

--- Runtime/Presentation/ActorSystem/ActorOverlayUIProvider.cs
+++ Runtime/Presentation/ActorSystem/ActorOverlayUIProvider.cs
@@ -2,6 +2,7 @@
 using Syadeu.Internal;
 using Syadeu.Presentation.Entities;
 using Syadeu.Presentation.Proxy;
+using Syadeu.Presentation.Render;
 using System;
 using System.Collections;
 using Unity.Mathematics;
@@ -11,27 +12,33 @@ namespace Syadeu.Presentation.Actor
 {
     public class ActorOverlayUIProvider : ActorProviderBase
     {
+        [Flags]
         public enum UpdateType
         {
-            Manual,
+            Manual          =   0,
             
-            Lerp,
-            Instant
+            Lerp            =   0b001,
+            Instant         =   0b010,
+
+            SyncOrientation =   0b100,
         }
         private struct UpdateJob : ICoroutineJob
         {
             private Entity<ActorEntity> m_Entity;
             private Entity<UIObjectEntity> m_UI;
-            private float3 m_Offset;
+            private float3 
+                m_Offset, m_OrientationOffset;
             private UpdateType m_UpdateType;
             private float m_UpdateSpeed;
 
-            public UpdateJob(Entity<ActorEntity> entity, Entity<UIObjectEntity> ui, float3 offset,
+            public UpdateJob(Entity<ActorEntity> entity, Entity<UIObjectEntity> ui, 
+                float3 offset, float3 orientationOffset,
                 UpdateType updateType, float updateSpeed)
             {
                 m_Entity = entity;
                 m_UI = ui;
                 m_Offset = offset;
+                m_OrientationOffset = orientationOffset;
                 m_UpdateType = updateType;
                 m_UpdateSpeed = updateSpeed;
             }
@@ -43,6 +50,9 @@ namespace Syadeu.Presentation.Actor
                 ITransform 
                     entityTr = m_Entity.transform,
                     uiTr = m_UI.transform;
+
+                RenderSystem renderSystem = PresentationSystem<RenderSystem>.System;
+                Transform camTr = renderSystem.Camera.transform;
 
                 while (true)
                 {
@@ -56,6 +66,19 @@ namespace Syadeu.Presentation.Actor
                             = math.lerp(uiTr.position, entityTr.position + m_Offset, Time.deltaTime * m_UpdateSpeed);
                     }
 
+                    if ((m_UpdateType & UpdateType.SyncOrientation) == UpdateType.SyncOrientation)
+                    {
+                        Quaternion orientation = Quaternion.LookRotation(camTr.forward, Vector3.up);
+                        if ((m_UpdateType & UpdateType.Instant) == UpdateType.Instant)
+                        {
+                            uiTr.rotation = orientation;
+                        }
+                        else if ((m_UpdateType & UpdateType.Lerp) == UpdateType.Lerp)
+                        {
+                            uiTr.rotation = Quaternion.Lerp(uiTr.rotation, orientation, Time.deltaTime * m_UpdateSpeed);
+                        }
+                    }
+
                     yield return null;
                 }
             }
@@ -63,13 +86,19 @@ namespace Syadeu.Presentation.Actor
 
         [JsonProperty(Order = 0, PropertyName = "Prefab")]
         protected Reference<UIObjectEntity> m_Prefab = Reference<UIObjectEntity>.Empty;
-        [JsonProperty(Order = 1, PropertyName = "Offset")]
-        protected float3 m_Offset = float3.zero;
-        [JsonProperty(Order = 2, PropertyName = "UpdateType")]
+        [JsonProperty(Order = 1, PropertyName = "UpdateType")]
         protected UpdateType m_UpdateType = UpdateType.Instant;
-        [JsonProperty(Order = 3, PropertyName = "UpdateSpeed")]
+        [JsonProperty(Order = 2, PropertyName = "UpdateSpeed")]
         protected float m_UpdateSpeed = 4;
 
+        [Space, Header("Position")]
+        [JsonProperty(Order = 3, PropertyName = "Offset")]
+        protected float3 m_Offset = float3.zero;
+
+        [Space, Header("Orientation")]
+        [JsonProperty(Order = 4, PropertyName = "OrientationOffset")]
+        protected float3 m_OrientationOffset = float3.zero;
+        
         [JsonIgnore] private Entity<UIObjectEntity> m_InstanceObject = Entity<UIObjectEntity>.Empty;
 
         [JsonIgnore] private UpdateJob m_UpdateJob;
@@ -96,7 +125,7 @@ namespace Syadeu.Presentation.Actor
 
             if (m_UpdateType != UpdateType.Manual)
             {
-                m_UpdateJob = new UpdateJob(entity, m_InstanceObject, m_Offset, m_UpdateType, m_UpdateSpeed);
+                m_UpdateJob = new UpdateJob(entity, m_InstanceObject, m_Offset, m_OrientationOffset, m_UpdateType, m_UpdateSpeed);
                 m_UpdateCoroutine = StartCoroutine(m_UpdateJob);
             }
         }
