b2d81196051f96383d25ef119f257c758761b0b8
Syadeu <syadeu@gmail.com>
2021-09-15 18:30:59 +0900

    fx 관리를 위해 entity 타입 추가

---
+++ Runtime/Presentation/Entities/FXEntity.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Utilities;
+using System.ComponentModel;
+using UnityEngine.Scripting;
+
+namespace Syadeu.Presentation.Entities
+{
+    [DisplayName("Entity: FX Entity")]
+    public sealed class FXEntity : EntityBase
+    {
+        [Preserve]
+        static void AOTCodeGeneration()
+        {
+            AotHelper.EnsureType<Instance<FXEntity>>();
+            AotHelper.EnsureList<Instance<FXEntity>>();
+            AotHelper.EnsureType<InstanceArray<FXEntity>>();
+
+            AotHelper.EnsureType<Reference<FXEntity>>();
+            AotHelper.EnsureList<Reference<FXEntity>>();
+            AotHelper.EnsureType<Entity<FXEntity>>();
+            AotHelper.EnsureList<Entity<FXEntity>>();
+            AotHelper.EnsureType<EntityData<FXEntity>>();
+            AotHelper.EnsureList<EntityData<FXEntity>>();
+            AotHelper.EnsureType<FXEntity>();
+            AotHelper.EnsureList<FXEntity>();
+        }
+    }
+}
