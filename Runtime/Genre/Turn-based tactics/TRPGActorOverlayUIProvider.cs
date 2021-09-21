f1502b6556c8b8a22154e10a647decc96220e794
Syadeu <syadeu@g1playground.com>
2021-09-21 14:32:10 +0900

    쉐이더 테스트

--- Runtime/Genre/Turn-based tactics/TRPGActorOverlayUIProvider.cs
+++ Runtime/Genre/Turn-based tactics/TRPGActorOverlayUIProvider.cs
@@ -1,9 +1,26 @@
-﻿using Syadeu.Presentation.Actor;
+﻿using Syadeu.Database;
+using Syadeu.Presentation.Actor;
+using UnityEngine;
 
 namespace Syadeu.Presentation.TurnTable
 {
     public sealed class TRPGActorOverlayUIProvider : ActorOverlayUIProvider
     {
+        //Texture2D temp;
 
+        protected override void OnCreated()
+        {
+            //temp = new Texture2D(1, 1);
+            //temp.SetPixels(new Color[] { Color.white });
+            //temp.Apply();
+
+            //GridExtensions.DefaultMaterial;
+
+            base.OnCreated();
+        }
+        private void DrawGrid()
+        {
+            
+        }
     }
 }
---
+++ Runtime/Presentation/RenderSystem/Shader/TestComputeShader.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestComputeShader : MonoBehaviour
+{
+    public int sphereAmount = 17;
+    public ComputeShader ComputeShader;
+
+    ComputeBuffer resultBuffer;
+    int kernel;
+    uint threadGroupSize;
+    Vector3[] output;
+    Transform[] transforms;
+
+    private void Start()
+    {
+        kernel = ComputeShader.FindKernel("CSMain");
+        ComputeShader.GetKernelThreadGroupSizes(kernel, out threadGroupSize, out _, out _);
+
+        resultBuffer = new ComputeBuffer(sphereAmount, sizeof(float) * 3);
+        output = new Vector3[sphereAmount];
+        transforms = new Transform[sphereAmount];
+
+        for (int i = 0; i < sphereAmount; i++)
+        {
+            transforms[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+        }
+    }
+    private void OnDestroy()
+    {
+        resultBuffer.Dispose();
+    }
+
+    private void Update()
+    {
+        ComputeShader.SetBuffer(kernel, "Result", resultBuffer);
+        int threadGroups = (int)((sphereAmount + (threadGroupSize - 1)) / threadGroupSize);
+        ComputeShader.Dispatch(kernel, threadGroups, 1, 1);
+        resultBuffer.GetData(output);
+
+        for (int i = 0; i < sphereAmount; i++)
+        {
+            transforms[i].position = output[i];
+        }
+    }
+}
