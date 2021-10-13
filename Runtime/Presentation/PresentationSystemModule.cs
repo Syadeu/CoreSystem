9e3d6603f545420823f63547a786ae90c61ba313
Syadeu <syadeu@gmail.com>
2021-10-13 15:11:23 +0900

    설명 수정

--- Runtime/Presentation/PresentationSystemModule.cs
+++ Runtime/Presentation/PresentationSystemModule.cs
@@ -7,8 +7,12 @@ using Syadeu.Presentation.Internal;
 namespace Syadeu.Presentation
 {
     /// <summary>
-    /// <see cref="PresentationSystemEntity{T}"/> 에 추가될 서브 모듈을 선언할 수 있습니다.
+    /// <typeparamref name="TSystem"/> 에 추가될 서브 모듈을 선언할 수 있습니다.
     /// </summary>
+    /// <remarks>
+    /// 모듈은 선언된 이후 <typeparamref name="TSystem"/> 가 <see cref="INotifySystemModule{TModule}"/> 을 상속받고
+    /// 이 모듈을 선언하여야 합니다.
+    /// </remarks>
     /// <typeparam name="TSystem"></typeparam>
     public abstract class PresentationSystemModule<TSystem> : PresentationSystemModuleBase
         where TSystem : PresentationSystemEntity
