43153b00c77c3640448996304f1a5938641461eb
Syadeu (Syadeu Home) <syadeu@gmail.com>
2021-11-18 00:49:51 +0900

    라이센스 업데이트

--- Runtime/Presentation/EventSystem/ISystemEventScheduler.cs
+++ Runtime/Presentation/EventSystem/ISystemEventScheduler.cs
@@ -1,4 +1,18 @@
-﻿namespace Syadeu.Presentation.Events
+﻿// Copyright 2021 Seung Ha Kim
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Syadeu.Presentation.Events
 {
     /// <summary>
     /// 시스템을 순차 수행 대상으로 선언할 수 있습니다.
