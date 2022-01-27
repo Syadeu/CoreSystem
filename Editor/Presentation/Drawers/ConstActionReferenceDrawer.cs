ba497f72687025796f319b2e73f15ca44f599691
Syadeu (Syadeu Home) <syadeu@gmail.com>
2022-01-28 01:30:42 +0900

    const action 추가

---
+++ Editor/Presentation/Drawers/ConstActionReferenceDrawer.cs
@@ -0,0 +1,103 @@
+﻿using Syadeu.Collections;
+using Syadeu.Presentation.Actions;
+using SyadeuEditor.Utilities;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace SyadeuEditor.Presentation
+{
+    public sealed class ConstActionReferenceDrawer : ObjectDrawer<IConstActionReference>
+    {
+        private static readonly Type[] s_ConstActionTypes;
+        static ConstActionReferenceDrawer()
+        {
+            s_ConstActionTypes = TypeHelper.GetTypes(t => !t.IsInterface && !t.IsAbstract &&
+                TypeHelper.TypeOf<IConstAction>.Type.IsAssignableFrom(t));
+        }
+
+        public ConstActionReferenceDrawer(object parentObject, MemberInfo memberInfo) : base(parentObject, memberInfo)
+        {
+        }
+        public ConstActionReferenceDrawer(IList list, int index, Type elementType) : base(list, index, elementType)
+        {
+        }
+        public ConstActionReferenceDrawer(object parentObject, Type declaredType, Action<IConstActionReference> setter, Func<IConstActionReference> getter) : base(parentObject, declaredType, setter, getter)
+        {
+        }
+
+        public override IConstActionReference Draw(IConstActionReference currentValue)
+        {
+            Type targetType;
+            Type[] generics = DeclaredType.GetGenericArguments();
+            if (generics.Length > 0) targetType = generics[0];
+            else targetType = null;
+
+            var iter = s_ConstActionTypes.Where(t => t.GUID.Equals(currentValue.Guid));
+            string targetName;
+            Type currentActionType = null;
+            if (iter.Any())
+            {
+                currentActionType = iter.First();
+                targetName = TypeHelper.ToString(currentActionType);
+            }
+            else
+            {
+                targetName = "None";
+            }
+
+            bool clicked;
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label(Name, GUILayout.Width(Screen.width * .25f));
+
+                clicked = EditorUtilities.BoxButton(targetName, ColorPalettes.PastelDreams.Mint, () =>
+                {
+                });
+            }
+
+            if (clicked)
+            {
+                DrawSelectionWindow((t) =>
+                {
+                    var ctor = TypeHelper.GetConstructorInfo(
+                        DeclaredType, TypeHelper.TypeOf<Guid>.Type);
+
+                    object temp;
+                    if (t == null)
+                    {
+                        temp = ctor.Invoke(new object[] { Guid.Empty });
+                    }
+                    else temp = ctor.Invoke(new object[] { t.GUID });
+
+                    Setter.Invoke((IConstActionReference)temp);
+
+                }, targetType);
+            }
+
+            return currentValue;
+        }
+
+        static void DrawSelectionWindow(Action<Type> setter, Type targetType)
+        {
+            Rect rect = GUILayoutUtility.GetRect(150, 300);
+            rect.position = Event.current.mousePosition;
+
+            var sort = s_ConstActionTypes.Where(t => t.BaseType.GenericTypeArguments[0].Equals(targetType)).ToArray();
+
+            PopupWindow.Show(rect, SelectorPopup<Type, Type>.GetWindow(
+                list: sort,
+                setter: setter,
+                getter: (t) =>
+                {
+                    return t;
+                },
+                noneValue: null,
+                other => TypeHelper.ToString(other)
+                ));
+        }
+    }
+}
---
+++ Runtime/Presentation/Actions/ConstAction.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2021 Seung Ha Kim
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#if (UNITY_EDITOR || DEVELOPMENT_BUILD) && !CORESYSTEM_DISABLE_CHECKS
+#define DEBUG_MODE
+#endif
+
+using Syadeu.Collections;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Syadeu.Presentation.Actions
+{
+    /// <summary>
+    /// Code-level 에서 미리 정의된 런타임 값을 반환하는 액션입니다.
+    /// </summary>
+    /// <remarks>
+    /// 모든 ConstAction 은 <see cref="GuidAttribute"/> 를 가지고 있어야 합니다.
+    /// </remarks>
+    /// <typeparam name="TValue"></typeparam>
+    public abstract class ConstAction<TValue> : IConstAction
+    {
+        Type IConstAction.ReturnType => TypeHelper.TypeOf<TValue>.Type;
+        object IConstAction.Execute() => Execute();
+
+        public ConstAction()
+        {
+        }
+
+        protected abstract TValue Execute();
+    }
+    public interface IConstAction
+    {
+        Type ReturnType { get; }
+        object Execute();
+    }
+
+    [Guid("0C11829E-730C-4082-B6E5-2ED487607F2E")]
+    public sealed class TestConstAction : ConstAction<int>
+    {
+        protected override int Execute()
+        {
+            "test const action".ToLog();
+            return 1;
+        }
+    }
+}
