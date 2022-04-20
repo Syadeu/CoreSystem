da6946f1903e1e1cfa95d97add7ea256947d2542
Syadeu <syadeu@gmail.com>
2022-04-20 19:25:03 +0900

    a

--- Runtime/Collections/Syadeu.Collections.Graphs/Unity/GetComponentNode.cs
+++ Runtime/Collections/Syadeu.Collections.Graphs/Unity/GetComponentNode.cs
@@ -1,4 +1,19 @@
-﻿using GraphProcessor;
+﻿// Copyright 2022 Seung Ha Kim
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using GraphProcessor;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Syadeu.Collections.Graphs
@@ -15,8 +30,18 @@ namespace Syadeu.Collections.Graphs
 
         protected override sealed void Process()
         {
-            if (target == null ||
-                !TypeHelper.InheritsFrom<UnityEngine.Object>(target.GetType())) return;
+            if (target == null)
+            {
+                //PointHelper.LogError(Channel.Core,
+                //    $"Input is null.");
+                return;
+            }
+            else if (!TypeHelper.InheritsFrom<UnityEngine.Object>(target.GetType()))
+            {
+                //PointHelper.LogError(Channel.Core,
+                //    $"Input({TypeHelper.ToString(target.GetType())}) is not inherits from {nameof(UnityEngine.Object)}. ");
+                return;
+            }
 
             if (target is GameObject gameObject)
             {
@@ -26,6 +51,23 @@ namespace Syadeu.Collections.Graphs
             {
                 output = component.GetComponent<TComponent>();
             }
+        }
+
+        [CustomPortInput(nameof(target), typeof(UnityEngine.Object))]
+        public void PullInputs(List<SerializableEdge> edges)
+        {
+            //"pull input executed".ToLog();
+
+            if (edges.Count != 1)
+            {
+                target = null;
+                return;
+            }
+
+            target = (UnityEngine.Object)edges[0].passThroughBuffer;
+
+            ParameterNode node = (ParameterNode)edges[0].outputNode;
+            //$"{node.parameterGUID} :: {node.parameter?.ToString()} :: {node.parameter?.value} :: {edges[0].passThroughBuffer}".ToLog();
         }
     }
 }
